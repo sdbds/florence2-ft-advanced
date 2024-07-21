0d2d2ca31e6390283b0a9faa597c19e03e0d2bbc
sdbds <865105819@qq.com>
2024-07-21 23:58:39 +0800

    Update install-cn-qinglong.ps1

--- install-cn-qinglong.ps1
+++ install-cn-qinglong.ps1
@@ -31,9 +31,9 @@ Check "激活虚拟环境失败。"
 Write-Output "安装程序所需依赖 (已进行国内加速，若在国外或无法使用加速源请换用 install.ps1 脚本)"
 $install_torch = Read-Host "是否需要安装 Torch+xformers? 若您本次为首次安装请选择 y ，若本次为升级依赖安装则选择 n。[y/n] (默认为 y)"
 if ($install_torch -eq "y" -or $install_torch -eq "Y" -or $install_torch -eq ""){
-    pip install torch==2.3.0+cu121 torchvision==0.18.0+cu121 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
+    pip install torch==2.3.1+cu121 torchvision==0.18.1+cu121 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html
     Check "torch 安装失败，请删除 venv 文件夹后重新运行。"
-    pip install --no-deps xformers==0.0.26.post1
+    pip install --no-deps xformers==0.0.27
     Check "xformers 安装失败。"
 }
 
